551d165072b1680c8c347f3cfe5c26c8d3c202e2
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-13 13:01:48 -0600

    Add     "start": "node --watch app/index.js",

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
+
+app.listen(3000, () => {
+  console.info("Server is running on port 3000");
+});
