c61cb22fb45d01170292325445c5e359b2d87bb7
Ed Marciniak <ekmarciniak@gmail.com>
2023-01-17 10:35:44 -0600

    Add get use post

--- app/index.js
+++ app/index.js
@@ -6,6 +6,16 @@ app.get("/", (req, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
+app.get("/about", (req, response) => {
+  response.send("<h1>About page!</h1>");
+});
+
+app.use(express.json());
+
+app.post("/api/contacts", (request, response) => {
+  response.send("ok");
+});
+
 app.listen(3000, () => {
   console.info("Server is running on port 3000");
 });
